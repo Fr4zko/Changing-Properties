6de16394c3dcf15d2f0df12666af841a44d76490
Fr4zko <91815132+Fr4zko@users.noreply.github.com>
2025-08-29 17:57:07 -0300

    Create Script.lua
    Mudanças

---
+++ Script.lua
@@ -0,0 +1,8 @@
+local Parede = game.Workspace.Wall
+
+Parede.BrickColor = BrickColor.random()
+Parede.Anchored = true
+Parede.Material = "Granite"
+Parede.Transparency = 0.3
+
+-- Coloca no ServerScriptService
